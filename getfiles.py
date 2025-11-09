7fa7452d4dc213490d6d50b63b3f3bc0966b6879
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2025-11-09 11:54:14 +0530

    create file to get images from kaggle as filenames

--- getfiles.py
+++ getfiles.py
@@ -1,7 +1,32 @@
 # Gets a list of images of license plates from kaggle for the purpose of testing
 import kagglehub
+import os
+import cv2
+import matplotlib.pyplot as plt
 
-# Download latest version
-path = kagglehub.dataset_download("andrewmvd/car-plate-detection")
+# Returns a list of pathnames of images and download if necessary
+def get_images():
+  # Download latest version of the images via kaggle
+  path = kagglehub.dataset_download("andrewmvd/car-plate-detection")
+  # print("Path to dataset files:", path)
 
-print("Path to dataset files:", path)
+  image_dir = path + '/images'
+  image_files = os.listdir(image_dir)
+
+  images = []
+  for image in image_files:
+    images.push(image_dir + "/" + image)
+  return images
+
+
+ex_image = cv2.imread(image_dir + "/" + image_files[3])
+plt.imshow(ex_image)
+plt.show()
+
+
+
+
+# if not image_files:
+#   print('working')
+# else:
+#   print('not working')
