8f86e14145f0f5a251c3059455455fe957823987
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2025-11-09 13:32:40 +0530

    update

--- checkplate.py
+++ checkplate.py
@@ -6,8 +6,14 @@ import run_ocr
 
 
 images = get_images()
-for image_path in images:
+for image_path in images[5:15]:
   print(run_ocr.get_plate(image_path))
+  image = cv2.imread(image_path)
+  plt.imshow(image)
+  plt.show()
+
+# image_path = images[1]
+# print(run_ocr.get_plate(image_path))
 
 
 # image = cv2.imread(image_path)
