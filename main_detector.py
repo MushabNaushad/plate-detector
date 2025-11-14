6ad758d02a93c676746667d0fe1ed302d2a546b6
MushabNaushad <ahamedmushab9@gmail.com>
2025-11-15 00:06:10 +0530

    Added Plate Pattern Finder

--- main_detector.py
+++ main_detector.py
@@ -1,9 +1,13 @@
 import cv2
 from paddleocr import PaddleOCR
 from ultralytics import YOLO
+import re
+from collections import Counter
 
 
 def recognize_plate(image):
+    ''' This Function recognizes the numberplate and return the cropped image of the plate'''
+
     # initialize recognizer and get results
     results = recognizer(image)
 
@@ -36,11 +40,33 @@ def detect_text(image)->list:
     texts = []
     text = results[0]['rec_texts']
     if text:
-        if type(text) == list:
-            text = text[0]
         texts.append(text)
 
     return texts
+
+def find_plate(texts:list,plate:list)->bool:
+    ''' This function finds whether the plate is read by the OCR '''
+
+    jumbled_plate = str()
+    # split any words inorder to match
+    for text in texts:
+        words = []
+        if '-' in text:
+            words = text.split('-')
+        if ' ' in text:
+            words = text.split()
+        if words:
+            for word in words:
+                jumbled_plate += word
+        else:
+            jumbled_plate += text
+    
+    pattern_of_plate = '|'.join(plate)
+    parts = re.findall(rf'({pattern_of_plate})',jumbled_plate)
+    
+    Found_plate = Counter(parts) == Counter(plate)
+
+    return Found_plate
 
 video = cv2.VideoCapture(1)
 
@@ -53,7 +79,10 @@ reader = PaddleOCR(
 frame_counter = 0
 
 output = []
-plates = ['WP QR 5029','CP BJA 1482']
+plates = [
+    ['WP', 'QR' ,'5029'],
+    ['CP','BJA','1482']
+]
 
 while True:
     
@@ -66,22 +95,18 @@ while True:
         
         #we take the first(and only) results since only one plate appears for our purpose
         texts = detect_text(cropped[0])
-        
-        
         for plate in plates:
-            # splitting the plate numbers, since it doesn't read as a group
-            splitted = plate.split()
-            found = False
-            for word in splitted:
-                if word in texts:
-                    found = True
-                else:
-                    found = False
-            
-            if found:
-                output.append(1)
+            isFound = find_plate(texts,plate)
+            if isFound:
                 break
         else:
+            isFound = False
+        
+        if isFound:
+            output.append(1)
+        else:
             output.append(0)
+
+        
 
 print('successful')
