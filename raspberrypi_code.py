87356dca0116c80aed238eaf2eec0a7fe230fa12
MushabNaushad <ahamedmushab9@gmail.com>
2025-11-23 15:07:18 +0530

    Final OCR change

--- raspberrypi_code.py
+++ raspberrypi_code.py
@@ -1,15 +1,17 @@
 import cv2
-from picamera2 import Picamera2
 from paddleocr import PaddleOCR
 from ultralytics import YOLO
 import re
 from collections import Counter
 import sys
+from picamera2 import Picamera2
+import RPi.GPIO as GPIO
+import time
 
 def cleanup():
     camera.stop()
-
-    
+    GPIO.setmode(GATE_PIN, GPIO.LOW)
+    GPIO.cleanup()
 
 def plate_recognizer(image):
     ''' This Function recognizes the numberplate and return the cropped image of the plate'''
@@ -33,108 +35,119 @@ def plate_recognizer(image):
 
             probability = box.conf.tolist()[0]
             # set the threshold probability
-            if probability > 0:
+            if probability > 0.5:
                 cropped_img = image[y1:y2,x1:x2]
                 cropped_images.append(cropped_img)
 
     return cropped_images
 
-def read_text(image)->list:
-    ''' This function outputs Reads the Numberplate '''
-
-    results = reader.predict(image)
-    text = results[0]['rec_texts']
-    if text:
-        return text
-    else:
+def read_text(image) -> list:
+    ''' This function reads the Numberplate and returns a list of strings '''
+    
+    # read the plate
+    results = reader.ocr(image, cls=False) 
+    if not results or results[0] is None:
         return None
+
+    detected_lines = results[0]
+    final_texts = []
+
+    # turn all the detected text to uppercase and return a list of text
+    for line in detected_lines:
+        text_content = line[1][0].upper()
+        final_texts.append(text_content)
+    
+    # output the strings that may or may not be in the actual order
+    return final_texts
 
 def find_plate(texts:list,plate:list)->bool:
     ''' This function finds whether the correct plate is read by the OCR '''
 
     jumbled_plate = str()
-    # split any words inorder to match
     for text in texts:
-        words = []
-        if '-' in text:
-            words = text.split('-')
-        if ' ' in text:
-            words = text.split()
-        if words:
-            for word in words:
-                jumbled_plate += word
-        else:
-            jumbled_plate += text
+        # remove -, spaces from the detected text
+        alphanum_in_plate = text.replace('-', '').replace(' ', '')
+        jumbled_plate += alphanum_in_plate
     
+    # find any permutation of the letters and numbers given to be identical to the plate
     pattern_of_plate = '|'.join(plate)
-    parts = re.findall(rf'({pattern_of_plate})',jumbled_plate)
+    parts = re.findall(rf'({pattern_of_plate})', jumbled_plate)
     
     Found_plate = Counter(parts) == Counter(plate)
 
     return Found_plate
             
-     
-# initialize Camera
-camera = Picamera2()
-camera.configure(camera.create_video_configuration(main={"size": (640, 640)}))
-camera.start()
+if __name__ == '__main__':
+    # initialize Camera
+    camera = Picamera2()
+    camera.configure(camera.create_video_configuration(main={"size": (640, 640)}))
+    camera.start()
 
-# initialize Model
-recognizer = YOLO('modelv2.pt')
-reader = PaddleOCR(
-    use_textline_orientation=True,
-    lang='en'
-)
+    # initialize Model
+    reader = PaddleOCR(use_angle_cls=False, lang="en", show_log=False)  
+    recognizer = YOLO('modelv2.pt')
 
-# control variables
-frame_counter = 0
-check_frame = 20
-allowed_plates = [
-	['CP','VS','4035']
-]
-try:
-    while True:
-        frame_4_ch = camera.capture_array()
-        if frame_4_ch is not None:
-            frame = cv2.cvtColor(frame_4_ch,cv2.COLOR_RGBA2RGB)
-            frame_counter += 1
-    
-            if frame_counter % check_frame == 0:
-                frame_counter = 0
-                cropped_set = plate_recognizer(frame)
-                # taking the best possible set
-	    
-                if cropped_set:
-                    plate = cropped_set[0]
-                    if plate.size == 0 or plate.shape[0] <= 0 or plate.shape[1] <= 0:
-                        print("Skipping empty or invalid crop.")
-                        continue
+    # control variables
+    frame_counter = 0
+    check_frame = 20
+    allowed_plates = [
+        ['CP','MQ','5196']
+    ]
+    GATE_PIN = 17
+    Gate_Position = False # True = Open and False = closed
 
-                    ocr_results = read_text(plate)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GATE_PIN, GPIO.OUT)
+    GPIO.setmode(GATE_PIN, GPIO.LOW)
 
-                    if ocr_results:
-                        for plate in allowed_plates:
-                            found = find_plate(ocr_results,plate)
-                            if found:
-                                break
-                            else:
-                                found = False
+    try:
+        while True:
+            frame_4_ch = camera.capture_array()
+            if frame_4_ch is not None:
+                frame = cv2.cvtColor(frame_4_ch, cv2.COLOR_RGBA2RGB)
+                frame_counter += 1
         
-                        if found:
-                            print('OPEN GATE')
-                        else:
-                            print('Wrong VEHICLE')
+                if frame_counter % check_frame == 0:
+                    frame_counter = 0
+                    cropped_set = plate_recognizer(frame)
+                    # taking the best possible set
             
-                else:
-                    print('NO VEHICLE')
-		 
-	    #cv2.imshow('Live',frame)
-	
-	    #if cv2.waitKey(1) & 0xFF == ord('q'):
-	    #	break
-except KeyboardInterrupt:
-    sys.exit(0)
-except Exception as e:
-    print(f'{e}')
-finally:
-    cleanup()
+                    if cropped_set:
+                        plate = cropped_set[0]
+                        # check whether the plate is image is empty
+                        if plate.size == 0 or plate.shape[0] <= 0 or plate.shape[1] <= 0:
+                            print("Skipping empty or invalid crop.")
+                            continue
+                        
+                        ocr_results = read_text(plate)
+
+                        if ocr_results:
+                            # check for each allowed plates
+                            for plate in allowed_plates:
+                                found = find_plate(ocr_results, plate)
+                                if found:
+                                    break
+                                else:
+                                    found = False
+            
+                            if found:
+                                print('OPEN GATE')
+                                GPIO.setmode(GATE_PIN, GPIO.HIGH)
+                                Gate_Position = True
+
+                            else:
+                                print('Wrong VEHICLE')
+                
+                    else:
+                        print('NO VEHICLE')
+                        if Gate_Position:
+                            time.sleep(20)
+                            Gate_Position = False
+            else:
+                break
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception as e:
+        print(f'{e}')
+    finally:
+        cleanup()
